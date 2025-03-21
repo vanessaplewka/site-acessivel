9ad5a51e45a3fdea6ae872bf30a0a258ad65b1ab
vanessaplewka <106452280+vanessaplewka@users.noreply.github.com>
2025-03-21 11:36:07 -0300

    botão acessivel

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
+        
     })
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
